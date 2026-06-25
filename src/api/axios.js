b7b4afe94237f47b3310030138677dd003b4e05f
Ayush Kumar Singh <aj945892@gmail.com>
2026-06-25 10:27:36 +0530

    add baseURL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://helpdesk-backend-pn7r.onrender.com/api',
 });
 
 // Interceptor
